84544039aed6d2734c9512b50ca74313b378f2e2
aarav-singhal035 <aarav.singhal@stu.ocsb.ca>
2025-09-22 17:48:54 -0400

    its clears the screen then show the temperature in celsius

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Aarav Singhal
- * Created on: Sep 2025
- * This program shows the temperature
-*/
+/* Copyright (c) 2020 MTHS All rights reserved * * Created by: Aarav Singhal * Created on: Sep 2025 * This program shows the temperature */
+
+
+
+
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
+// Declare the temperature variable at the top
+let temp: number;
 
-// comment
-input.onButtonPressed(Button.A,() => {
-    let temp = input.temperature()
+input.onButtonPressed(Button.A, () => {
+    temp = input.temperature()
     basic.showNumber(temp)
 })
