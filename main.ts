10be61015a2db4b9c8fe3f3facf452fdec5dc5c7
aarav-singhal035 <aarav.singhal@stu.ocsb.ca>
2025-09-24 13:17:42 -0400

    made it in celsius

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved * * Created by: Aarav Singhal * Created on: Sep 2025 * This program shows the temperature */
 
+// our variable for temperature in Celsius
+let temperatureInCelsius: number
+
 
 
 
@@ -7,6 +10,9 @@
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
+input.onButtonPressed(Button.A,() => {
+    temperatureInCelsius = input.temperature()
+    basic.showNumber(temperatureInCelsius)
 // Declare the temperature variable at the top
 let temp: number;
 
